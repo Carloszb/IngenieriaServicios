f90b441630e5171e0962a7ec5bdc750d53531bf5
medinafdzz <suso121163@gmail.com>
2025-12-03 17:29:53 +0100

    Practica 7 final

--- Practica7/app/src/main/java/es/uniovi/converter/MainActivity.kt
+++ Practica7/app/src/main/java/es/uniovi/converter/MainActivity.kt
@@ -4,14 +4,15 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    private var euroToDollar: Double = 1.16
     private lateinit var editTextEuros: EditText
     private lateinit var editTextDollars: EditText
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,11 +22,14 @@ class MainActivity : AppCompatActivity() {
         editTextEuros = findViewById(R.id.editTextEuros)
         editTextDollars = findViewById(R.id.editTextDollars)
 
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        viewModel.fetchExchangeRate()
     }
 
     private fun convert(source: EditText, destination: EditText, factor: Double) {
@@ -39,10 +43,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onClickToDollars(view: View) {
-        convert(editTextEuros, editTextDollars, euroToDollar)
+        val factor = viewModel.euroToDollar
+        convert(editTextEuros, editTextDollars, factor)
     }
 
     fun onClickToEuros(view: View) {
-        convert(editTextDollars, editTextEuros, 1 / euroToDollar)
+        val factor = viewModel.euroToDollar
+        convert(editTextDollars, editTextEuros, 1 / factor)
     }
 }
